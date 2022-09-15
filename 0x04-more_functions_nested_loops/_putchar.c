32a34f27a812154eaf2b1c5f68739cdb54af78fb
realvoub <benjaminvictor64@yahoo.com>
2022-09-15 14:45:37 -0700

    betty

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,27 @@
+nclude <unistd.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _putchar - writes the chracter c to stdout
+ *
+ *   * @c: The character to print
+ *
+ *    *
+ *
+ *     * Return: On success 1.
+ *
+ *      * on error, -1 is returned.
+ *
+ *       */
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
