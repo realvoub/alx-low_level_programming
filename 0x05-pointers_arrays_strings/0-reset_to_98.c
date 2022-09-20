33b7c88cab1fcef2ae314e7da29f8a2dd8d122ec
realvoub <benjaminvictor64@yahoo.com>
2022-09-20 19:38:52 +0000

    voub

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,15 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#include "main.h"
 
-int _putchar(char);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-#endif
+/**
+ * reset_to_98 - takes a ponter to an int and points to 98.
+ * @n: integer value
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
