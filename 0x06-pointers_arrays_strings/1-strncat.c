6bb4e4315479140b817c08a43d9ec489d5544a59
realvoub <benjaminvictor64@yahoo.com>
2022-09-22 20:48:46 +0000

    victor

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,10 +7,12 @@
  * @n: integer
  * Return: dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
-while (*(dest + count) != '\0')
+
+	while (*(dest + count) != '\0')
 	{
 		count++;
 	}
