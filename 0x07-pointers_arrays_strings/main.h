118cb1195f28d5200180f89690c42c4d1d492f9f
realvoub <benjaminvictor64@yahoo.com>
2022-09-26 22:25:31 +0000

    ufot

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -26,4 +27,20 @@ void simple_print_buffer(char *buffer, unsigned int size)
                 i++;
         }
         printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
+    return (0);
 }
