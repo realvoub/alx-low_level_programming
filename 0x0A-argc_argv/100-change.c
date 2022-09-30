64e8513035591c09437027e70df3444a5b38f3d9
realvoub <benjaminvictor64@yahoo.com>
2022-09-30 22:02:11 +0000

    faithjob

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,45 +1,65 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 /**
- * main - adds positive numbers
- * @argc: number of arguments
+ * main - Entry point
+ * @argc: arguments
  * @argv: array of arguments
- * Return: o if no number is passed 1 print Error.
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-	unsigned int k, sum = 0;
-	char *e;
-
-	if (argc > 1)
+	if (argc != 2)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			e = argv[i];
-
-			for (k = 0; k < strlen(e); k++)
-			{
-				if (e[k] < 48 || e[k] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			sum += atoi(e);
-			e++;
-		}
-
-		printf("%d\n", sum);
+		printf("%s\n", "Error");
+		return (1);
 	}
-	else
+	else if (argc < 0)
 	{
-		printf("0\n");
+		return (0);
 	}
 
+	printf("%d\n", change(atoi(argv[1])));
 	return (0);
+}
+
+/**
+ * change - get change
+ * @cents: amount of coins from main function
+ * Return: change
+ */
+int change(int cents)
+{
+	int q = 25, d = 10, n = 5, t = 2, p = 1;
+	int coins = 0;
+
+	while (cents > 0)
+	{
+		while (cents >= q)
+		{
+			cents -= q;
+			coins++;
+		}
+		while (cents >= d)
+		{
+			cents -= d;
+			coins++;
+		}
+		while (cents >= n)
+		{
+			cents -= n;
+			coins++;
+		}
+		while (cents >= t)
+		{
+			cents -= t;
+			coins++;
+		}
+		while (cents >= p)
+		{
+			cents -= p;
+			coins++;
+		}
+	}
+	return (coins);
 }
