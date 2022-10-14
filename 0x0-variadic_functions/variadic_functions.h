a55e67c01e610e6a2b9deff11f7f4bc9f4278422
realvoub <benjaminvictor64@yahoo.com>
2022-10-14 02:49:21 +0000

    status

--- 0x0-variadic_functions/variadic_functions.h
+++ 0x0-variadic_functions/variadic_functions.h
@@ -1,13 +1,22 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
-
+#ifndef VARFUNC_H
+#define VARFUNC_H
 #include <stdarg.h>
-#include <stdio.h>
-
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct type - Struct data type
+ *
+ * @op: data type argument
+ * @f: The function associated
+ */
+
+typedef struct type
+{
+	char *op;
+	void (*f)(va_list, char *);
+} type_t;
 
 #endif
