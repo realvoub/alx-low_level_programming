485a049a23fb70c61b064760d3e18b60c52b5d9c
realvoub <benjaminvictor64@yahoo.com>
2022-12-17 03:24:23 +0000

    dynamic

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,10 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int add(int a, int b);
-int mul(int a, int b);
-int sub(int a, int b);
-int div(int a, int b);
-int mod(int a, int b);
 
 #endif
